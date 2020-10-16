63de56c7da5044b9ef10ea9a86b8214ecb632e03
Aminat Puebla <ap2296@njit.edu>
2020-10-16 09:14:25 -0400

    initial

---
+++ knights_tour.js
@@ -0,0 +1,53 @@
+const board = []
+//this makes board with zeroes
+for(let i= 0; i< 5; i++){
+	board.push([]);
+	for(let j= 0; j< 5; j++){
+		board[i].push(0)
+	}
+}
+
+const pathRow = [2, 1, -1, -2, -2, -1, 1, 2];
+const pathCol = [1, 2, 2, 1, -1, -2, -2, -1];
+
+const findKnightsTour = (board, row, col, move ) =>{
+ /* A utility function to print solution matrix board[N][N] */
+  if(move  === 25){
+    for(let i = 0; i < 5; i ++){
+      for(let j = 0; j < 5; j ++){
+        continue;
+      }
+    }
+		console.log(board)
+		return true;
+
+	} else {
+		board[row][col] = move
+		for(let index= 0; index < pathRow.length; index++){
+			let newRow = row+pathRow[index];
+			let newCol = col+pathCol[index];
+			if(ifValidMove(board, newRow, newCol)){
+				move++
+
+        board[newRow][newCol] = move;
+        //this is the recursive part
+				if(findKnightsTour(board, newRow, newCol, move)){
+					return true;
+        }
+        //this is the bakctracking part
+				move--;
+				board[newRow][newCol] = 0
+			}
+		}
+	}
+	return false;
+}
+
+const ifValidMove = (board, newRow, newCol) => {
+	if(newRow >= 0 && newRow < 5 && newCol >= 0 && newCol < 5 && board[newRow] [newCol] === 0){
+		return true
+	}
+	return false
+}
+
+findKnightsTour(board, 0, 0, 1)
